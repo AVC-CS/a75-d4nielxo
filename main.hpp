5106a0bbfd7eeca82d157a599e5bdfcc635da149
d4niel <dhaddad3@students.avc.edu>
2026-05-14 21:55:11 -0700

    commit 1

--- main.hpp
+++ main.hpp
@@ -1,17 +1,42 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
-// Find the sub string in the original string
+
 
 #include <iostream>
 #include <cstring>
 using namespace std;
 
+
 int finduserstring(char[], int, char[], int);
+
 
 int finduserstring(char cstr[], int cstrlen, char userstr[], int userlen)
 {
-    // TODO: scan cstr for the first occurrence of userstr, return its starting
+    if (userlen > cstrlen)
+        return -1;
+
+    for (int i = 0; i <= cstrlen - userlen; ++i)
+    {
+        bool match = true;
+
+        for (int j = 0; j < userlen; ++j)
+        {
+            if (cstr[i + j] != userstr[j])
+            {
+                match = false;
+                break;
+            }
+        }
+
+        if (match)
+        {
+            return i;
+        }
+    }
+
+    return -1;
 }
+
 
 #endif
